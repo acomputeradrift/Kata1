5ff5445067e81f418591399d799206e99916370b
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-21 13:26:52 -0700

    I have split the multiple numbers and the displa numbers into 2 mutable arrays but can't get my loops to work

--- Kata1/NumberRepeater.m
+++ Kata1/NumberRepeater.m
@@ -20,24 +20,88 @@
 
 - (NSString *)process
 {
+    
+    //need fast enumeration to do this for all the items in the array
+    
+    
     //Unpackage the multiplier for the for loop as an NSNumber
-    NSNumber *multiplier = _toBeRetreivedLater[0][1];
-    //For some reason I have to turn it into an NSInteger for use
-    NSInteger intMultiplier = [multiplier integerValue];
-    // Break out the display number for adding to new array a certain number of times
-    NSNumber *displayNumber = _toBeRetreivedLater[0][0];
+    
+    // this loop gets me the multiple number
+    int row =0;
+    int column = 1;
+    NSMutableArray *multiplierArray = [NSMutableArray new];
+    for (NSArray *count in _toBeRetreivedLater)
+    {
+        //NSLog (@"%@", _toBeRetreivedLater[row][column]); //proves where the single number is
+        NSNumber *multiplier = _toBeRetreivedLater[row][column]; //loads one object into variable
+        //NSInteger intMultiplier = [multiplier integerValue]; // converts one object to comparable number for for loop
+        [multiplierArray addObject:multiplier];//adds the one number to the new array
+        row = row+1;
+    }
+    
+    NSLog (@"%@", multiplierArray);
+    row=0;
+    column=0;
+    
+    
+    // this for loop gets me the display number
+    for (NSArray *count in _toBeRetreivedLater)
+    {
+        //NSLog (@"%@", _toBeRetreivedLater[row][column]);  //proves the display number
+        NSNumber *displayNumber = _toBeRetreivedLater[row][column]; //pulls display numbers out
+        // load the display objects into an separate array
+         NSMutableArray *displayArray = [NSMutableArray new]; //creates array
+        
+        
+        // pull the first number out of the arraygi
+            for (NSNumber *count in multiplierArray) //get intMuliplier from other array
+            {
+                
+               //load the displayNumber into an mutable array as objects
+                [displayArray addObject:displayNumber];
+            }
+        row = row+1;
+        NSLog (@"%@", displayArray);
+    }
+    
 
     
-    NSMutableArray *displayArray = [NSMutableArray new];
-    for (int i = 0; i < intMultiplier; i++)
-    {
-       //load the displayNumber into an mutable array as objects
-        [displayArray addObject:displayNumber];
-    }
-    // print the array as a string
-    NSString *printedResults = [displayArray componentsJoinedByString:@""];
-    NSLog (@"%@", printedResults);
-    return @"Test";
+//    for (NSArray *count2 in _toBeRetreivedLater)
+//    {
+//        NSLog (@"%@", _toBeRetreivedLater[row][column]);
+//        column = column+1;
+//    }
+    //NSNumber *multiplier = _toBeRetreivedLater[row][column];
+   // NSString *whatsInTheArray = [_toBeRetreivedLater componentsJoinedByString:@""];
+   // NSLog (@"%@", whatsInTheArray);
+    
+    
+    //For some reason I have to then turn it into an NSInteger for use
+    //NSInteger intMultiplier = [multiplier integerValue];
+   
+    
+    // Break out the display number for adding to new array a certain number of times
+   // NSNumber *displayNumber = _toBeRetreivedLater[0][0];
+   
+    //create the new array for only the display number
+   // NSMutableArray *displayArray = [NSMutableArray new];
+    
+    
+//    for (int i = 0; i < intMultiplier; i++)
+//    {
+//       //load the displayNumber into an mutable array as objects
+//        [displayArray addObject:displayNumber];
+//    }
+//
+//    // print the array as a string
+//    NSString *printedResults = [displayArray componentsJoinedByString:@""];
+//    NSLog (@"%@", printedResults);
+    
+
+    return @"Process has been run for first array";
 }
 
 @end
+// Ok what does this have to do?
+    
+    
