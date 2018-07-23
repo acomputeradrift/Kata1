f74398f3a8588a93bd069024ce2dbb57eaf9f2f1
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-22 19:17:31 -0700

    Refined with less lines and more comments

--- Kata1/NumberRepeater.m
+++ Kata1/NumberRepeater.m
@@ -10,52 +10,47 @@
 
 @implementation NumberRepeater
 
-- (instancetype)initWithNumbers:(NSArray *)numbers
+- (instancetype)initWithNumbers:(NSArray *)numbers //hopefully this loads the property with the array
 {
-    //hopefully this loads the property with the array
-    
     self.toBeRetreivedLater = numbers;
     return self;
 }
 
 - (NSString*)process
 {
+    //--------------------------------------
     int row =0;
     NSMutableArray *multiplierArray = [NSMutableArray new];
     
-     // this loop gets me the multiple number
-    for (NSArray *count in _toBeRetreivedLater)
+    
+    for (NSArray *count in _toBeRetreivedLater) // this loop gets me the multiple number only from the 2d array
     {
-        NSNumber *multiplier = _toBeRetreivedLater[row][1]; //loads one object into variable each time
-        [multiplierArray addObject:multiplier];//adds the one number to the new array
-        row = row+1;
+        NSNumber *multiplierNumber = _toBeRetreivedLater[row][1]; //pulls one multiplierNumber object out each time as NSNumber
+        [multiplierArray addObject:multiplierNumber];//adds the one multiplierNumber as an object to the new array in the last position
+        row = row+1; //repeats the loop for the next index in the 2d array
     }
     
+     //---------------------------------------
     
     row=0;
-    NSMutableArray *displayArray = [NSMutableArray new]; //creates array
+    NSMutableArray *displayArray = [NSMutableArray new]; //creates mutable array
 
-    // this for loop gets me the display number
-    for (NSArray *count in _toBeRetreivedLater)
+    for (NSArray *count in _toBeRetreivedLater)  // this loop gets me the display number only from the 2d array
     {
-        NSNumber *displayNumber = _toBeRetreivedLater[row][0]; //pulls display numbers out
-        NSNumber *multiplier2 = multiplierArray[row];// pull the first number out of the array and use it for the loop
-        NSInteger intMultiplier = [multiplier2 integerValue];
+        NSNumber *displayNumber = _toBeRetreivedLater[row][0]; //pulls one displayNumber object out each time as NSNumber
+        NSInteger intMultiplier = [multiplierArray[row] integerValue];// pull the first number out of the new array and uses it for the loop as an NSInteger
+        
         if (row >= 1) //placement of the comma
         {
             [displayArray addObject:@","];
         }
-       
-        for (int i = 0; i < intMultiplier; i++) //run this loop as many times as the value of the multiplier
+        for (int i = 0; i < intMultiplier; i++) //run this loop as many times as the value of the multiplier you got from the first for in loop
             {
-                [displayArray addObject:displayNumber];//load the displayNumber into an mutable array as objects
-                
+                [displayArray addObject:displayNumber];//adds the one Displaynumber as an object to the new array in the last position
             }
-        row = row+1;
-       
+        row = row+1;//repeats the loop for the next index in the 2d array
     }
-   NSString *returnString = [displayArray componentsJoinedByString:@""];
-    //NSLog (@"%@", [displayArray componentsJoinedByString:@""]);
+    NSString *returnString = [displayArray componentsJoinedByString:@""]; //adds the contents of the displayArray to an NSString
     return returnString;
 }
 
